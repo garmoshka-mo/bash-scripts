dbee08a8aa19aa451d973412fb54a721915ff110
Daniel <245dany@gmail.com>
2025-08-30 17:30:37 +0200

    clean

--- scripts/com.sh
+++
@@ -1,9 +0,0 @@
-if [[ `pwd`/ = */hub/* ]] 
-then
- echo Dont push hub from here!
-else
- git add -A
- git commit -m "$@"
-fi
-
- 
--- scripts/echo.sh
+++
@@ -1,9 +0,0 @@
-if [[ `pwd`/ = */hub/* ]]
-then
-  echo 'Dont do it!!!'
-else
-  echo 'ok'
-fi
-
-echo `pwd`
-echo 'params:'$@
